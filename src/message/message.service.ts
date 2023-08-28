74de333330eb57a5e0472e616ddc3ec0206b2ba2
aki <agosight0307@gmail.com>
2023-08-28 16:26:22 +0900

    投稿機能

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -1,11 +1,85 @@
-import { Injectable } from '@nestjs/common';
-
-// 追記
+import { Injectable, ForbiddenException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+
 import { Message } from '@prisma/client';
 
 @Injectable()
 export class MessageService {
-    // 追記
+    
     constructor(private prisma: PrismaService){}
+
+    getMessages(userId: number): Promise<Message[]>{
+        return this.prisma.message.findMany({
+            where: {
+                userId,
+            },
+            orderBy: {
+                createdAt: 'desc',
+            }
+        })
+    }
+
+    getMessageById(userId: number, messageId: number):Promise<Message>{
+        return this.prisma.message.findFirst({
+            where: {
+                userId,
+                id: messageId,
+            },
+        });
+    }
+
+    async createMessage(userId: number, dto: CreateMessageDto) :Promise<Message>{
+        const message = await this.prisma.message.create({
+            data: {
+                userId,
+                ...dto,
+            },
+        });
+        return message;
+    }
+
+    async UpdateMessageById(
+        userId: number,
+        messageId: number,
+        dto: UpdateMessageDto,
+    ): Promise<Message>{
+        const message = await this.prisma.message.findUnique({
+            where: {
+                id: messageId,
+            },
+        });
+
+        if(!message || message.userId !== userId){
+            throw new ForbiddenException('No permission to update');
+        }
+        return this.prisma.message.update({
+            where: {
+                id: messageId,
+            },
+            data: {
+                ...dto,
+            },
+        });
+
+    }
+
+    async deleteMessageById(userId: number, messageId: number){
+        const message = await this.prisma.message.findUnique({
+            where: {
+                id: messageId,
+            },
+        });
+
+        if(!message || message.userId !== userId) {
+            throw new ForbiddenException('No permission to delete');
+        }
+        await this.prisma.message.delete({
+            where: {
+                id: messageId,
+            }
+        })
+
+    }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService){}
+
+    @Get()
+    getLoginUser(@Req() req: Request): Omit<User, 'hashedPassword'> {
+        return req.user;
+    }
+}
